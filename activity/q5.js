dd08fe4aea9a41bba291e245433774a38e785491
vishesha841 <140616379+vishesha841@users.noreply.github.com>
2024-11-18 09:46:39 +0530

    Create q5.js

---
+++ activity/q5.js
@@ -0,0 +1,17 @@
+//Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+//Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+//Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+//Return k
+var removeElement = function(nums, val) {
+    let i = 0; 
+   
+    for (let j = 0; j < nums.length; j++) {
+        if (nums[j] !== val) {
+            nums[i] = nums[j]; 
+            i++; 
+        }
+    }
+    
+   
+    return i;
+};
